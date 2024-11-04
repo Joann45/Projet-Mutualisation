a31badfe628a3df681a16a78971a23cd82053d7e
lenny-vergerolle <vergerolle.lenny@gmail.com>
2024-11-04 11:56:43 +0100

    création de la page des réseaux

--- src/views.py
+++ src/views.py
@@ -9,3 +9,12 @@ def index():
 @app.route('/profil')
 def modifier_profil():
     return render_template('profil.html')
+
+@app.route('/mes_reseaux')
+def voir_mes_reseaux():
+    return render_template('voir_mes_reseaux.html')
+
+@app.route('/mes_reseaux_admin')
+def gerer_mes_reseaux():
+    return render_template('gerer_mes_reseaux.html')
+
