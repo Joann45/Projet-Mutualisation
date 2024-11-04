acf96870cbd6c46030a394056937afe51dc420db
lenny-vergerolle <vergerolle.lenny@gmail.com>
2024-11-04 17:00:16 +0100

    changement des routes

--- src/views.py
+++ src/views.py
@@ -17,14 +17,14 @@ def mdp_reset():
 def home():
     return render_template('home.html')
 
-@app.route('/profil')
+@app.route('/home/profil')
 def modifier_profil():
     return render_template('profil.html')
 
-@app.route('/mes_reseaux')
+@app.route('/home/mes_reseaux')
 def voir_mes_reseaux():
     return render_template('voir_mes_reseaux.html')
 
-@app.route('/mes_reseaux_admin')
+@app.route('/home/mes_reseaux_admin')
 def gerer_mes_reseaux():
     return render_template('gerer_mes_reseaux.html')
