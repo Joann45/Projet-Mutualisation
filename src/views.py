476a132e742d5ec74b9f00d53d69987cfa6f34a3
lenny-vergerolle <vergerolle.lenny@gmail.com>
2024-11-04 11:23:04 +0100

    changement des routes

--- src/views.py
+++ src/views.py
@@ -3,4 +3,9 @@ from flask import render_template, redirect, url_for, request
 
 @app.route('/')
 def index():
-    return render_template('profile.html')
+    return render_template('base.html')
+
+
+@app.route('/profil')
+def modifier_profil():
+    return render_template('profil.html')
