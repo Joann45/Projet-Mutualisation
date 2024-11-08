b1d821d0056ac2e08d9e5340529b9abe1e958048
RocmaDL <misterpoint0305@gmail.com>
2024-11-08 05:34:15 +0100

    implémentation de la gestion des utilisateurs, des genres, et des réseaux

--- src/forms/OffreForm.py
+++ src/forms/OffreForm.py
@@ -23,41 +23,41 @@ class OffreForm(FlaskForm):
     documents = FileField("Ajouter les documents liés à l'offre", validators=[DataRequired()])
     submit = SubmitField("Enregistrer")
 
-    def validate(self, extra_validators=None):
-        if not FlaskForm.validate(self, extra_validators=extra_validators):
-            return False
+    # def validate(self, extra_validators=None):
+    #     if not FlaskForm.validate(self, extra_validators=extra_validators):
+    #         return False
 
-        # Validation de budget par rapport à la cotisation minimale
-        if self.budget.data is not None and self.cotisation_min.data is not None:
-            if self.cotisation_min.data > self.budget.data:
-                self.budget.errors.append("Le budget doit être supérieur ou égal à la cotisation minimale.")
-                return False
+    #     # Validation de budget par rapport à la cotisation minimale
+    #     if self.budget.data is not None and self.cotisation_min.data is not None:
+    #         if self.cotisation_min.data > self.budget.data:
+    #             self.budget.errors.append("Le budget doit être supérieur ou égal à la cotisation minimale.")
+    #             return False
 
-        # Validation de la capacité minimale par rapport à la capacité maximale
-        if self.capacite_min.data is not None and self.capacite_max.data is not None:
-            if self.capacite_min.data > self.capacite_max.data:
-                self.capacite_min.errors.append("La capacité minimale doit être inférieure ou égale à la capacité maximale.")
-                return False
+    #     # Validation de la capacité minimale par rapport à la capacité maximale
+    #     if self.capacite_min.data is not None and self.capacite_max.data is not None:
+    #         if self.capacite_min.data > self.capacite_max.data:
+    #             self.capacite_min.errors.append("La capacité minimale doit être inférieure ou égale à la capacité maximale.")
+    #             return False
 
-        # Validation de la date limite par rapport à la date de début et aujourd'hui
-        if self.date_limite.data is not None:
-            if self.date_deb.data is not None and self.date_limite.data > self.date_deb.data:
-                self.date_limite.errors.append("La date limite doit être antérieure ou égale à la date de début.")
-                return False
-            if self.date_limite.data < date.today():
-                self.date_limite.errors.append("La date limite ne peut pas être dans le passé.")
-                return False
+    #     # Validation de la date limite par rapport à la date de début et aujourd'hui
+    #     if self.date_limite.data is not None:
+    #         if self.date_deb.data is not None and self.date_limite.data > self.date_deb.data:
+    #             self.date_limite.errors.append("La date limite doit être antérieure ou égale à la date de début.")
+    #             return False
+    #         if self.date_limite.data < date.today():
+    #             self.date_limite.errors.append("La date limite ne peut pas être dans le passé.")
+    #             return False
 
-        # Validation de la date de début par rapport à la date de fin
-        if self.date_deb.data is not None and self.date_fin.data is not None:
-            if self.date_deb.data > self.date_fin.data:
-                self.date_deb.errors.append("La date de début doit être antérieure ou égale à la date de fin.")
-                return False
+    #     # Validation de la date de début par rapport à la date de fin
+    #     if self.date_deb.data is not None and self.date_fin.data is not None:
+    #         if self.date_deb.data > self.date_fin.data:
+    #             self.date_deb.errors.append("La date de début doit être antérieure ou égale à la date de fin.")
+    #             return False
 
-        # Validation de la date de fin par rapport à la date de début
-        if self.date_fin.data is not None:
-            if self.date_deb.data is not None and self.date_fin.data < self.date_deb.data:
-                self.date_fin.errors.append("La date de fin doit être postérieure ou égale à la date de début.")
-                return False
+    #     # Validation de la date de fin par rapport à la date de début
+    #     if self.date_fin.data is not None:
+    #         if self.date_deb.data is not None and self.date_fin.data < self.date_deb.data:
+    #             self.date_fin.errors.append("La date de fin doit être postérieure ou égale à la date de début.")
+    #             return False
 
-        return True
+    #     return True
--- src/forms/ReseauForm.py
+++ src/forms/ReseauForm.py
@@ -1,7 +1,7 @@
 from collections.abc import Sequence
 from typing import Any, Mapping
 from flask_wtf import FlaskForm
-from wtforms import StringField, HiddenField, RadioField
+from wtforms import StringField, HiddenField, RadioField, SelectField
 from wtforms.validators import DataRequired
 from src.models.Reseau import Reseau
 from src.models.Utilisateur import Utilisateur
@@ -9,7 +9,7 @@ from src.models.Utilisateur import Utilisateur
 class ReseauForm(FlaskForm):
     id = HiddenField('id')
     nom_reseau = StringField('Nom du réseau', validators=[DataRequired()])
-    def validate(self):
+    def validate(self, extra_validators=None):
         if not FlaskForm.validate(self):
             return False
         if Reseau.query.filter_by(nom_reseau=self.nom_reseau.data).first():
@@ -19,19 +19,8 @@ class ReseauForm(FlaskForm):
 
 class AddUtilisateurReseauForm(FlaskForm):
     id = HiddenField('id')
-    reseau = StringField('Réseau', validators=[DataRequired()])
-    email_utilisateur = StringField('Email utilisateur', validators=[DataRequired()])
-    def validate(self):
-        if not FlaskForm.validate(self):
-            return False
-        if not Reseau.query.filter_by(nom_reseau=self.reseau.data).first():
-            self.reseau.errors.append('Ce réseau n\'existe pas')
-            return False
-        if not Utilisateur.query.filter_by(email_utilisateur=self.email_utilisateur.data).first():
-            self.email_utilisateur.errors.append('Cet utilisateur n\'existe pas')
-            return False
-        return True
-    
+    utilisateur = SelectField('Utilisateur', validators=[DataRequired()])
+
 class SelectReseauForm(FlaskForm):
     id = HiddenField('id')
     reseaux = RadioField('Réseaux', validators=[DataRequired()])
