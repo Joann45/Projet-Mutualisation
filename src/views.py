d0289a956e950dd46ae5e7fc5f74881ee751713c
lennyverg <vergerolle.lenny@gmail.com>
2024-12-18 14:17:14 +0100

    save

--- src/views.py
+++ src/views.py
@@ -262,6 +262,22 @@ def mes_reseaux_admin():
     reseau = Reseau.query.get(reseau_id)
     return render_template('mes-reseaux-admin.html', reseaux=les_reseaux, add_form=f_add_reseau, select_form=f_select_reseau, membres=[[membre.orga for membre in reseau.les_utilisateurs]], reseau_id=reseau_id)
 
+@app.route('/home/mes-offres/suppression-offre/<int:id_offre>', methods=['GET', 'POST'])
+def suppression_offre(id_offre):
+    """Supprime une offre
+
+    Args:
+        id_offre (int): L'identifiant de l'offre à supprimer
+
+    Returns:
+        mes-offres.html: Une page des offres de l'utilisateur
+    """
+    o = Offre.query.get(id_offre)
+    if o:
+        db.session.delete(o)
+        db.session.commit()
+    return redirect(url_for('mes_offres'),offre=o)
+
 @app.route('/home/mes-reseaux-admin/suppression_utilisateur/<int:id_reseau>/<int:id_utilisateur>', methods=['GET', 'POST'])
 def suppression_utilisateur_reseau(id_reseau, id_utilisateur):
     """Supprime un utilisateur d'un réseau
