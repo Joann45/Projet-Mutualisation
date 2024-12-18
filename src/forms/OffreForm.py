d60610edd61856a51522f5689486327bac44d81b
RocmaDL <misterpoint0305@gmail.com>
2024-12-18 12:38:22 +0100

    Implémentation des popups bootstrap

--- src/forms/OffreForm.py
+++ src/forms/OffreForm.py
@@ -13,7 +13,7 @@ class OffreForm(FlaskForm):
     capacite_min = IntegerField('Capacité minimal', validators=[DataRequired()])
     capacite_max = IntegerField('Capacité maximal', validators=[DataRequired()])
     img = FileField("Image de l'offre", validators=[DataRequired()])
-    etat = StringField("État de l'offre", validators=[DataRequired()])
+    etat = StringField("État de l'offre")
     genre = SelectField('Genre', coerce=int)
     reseau = SelectField('Réseaux', coerce=int)
     nom_loc = StringField('Localisation', validators=[DataRequired()])
