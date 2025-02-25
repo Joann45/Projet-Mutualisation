e1e02a665609dfe7df1805288e7539cfa6e640c8
varuniv <sargis.vardanyan@etu.univ-orelans.fr>
2025-02-25 12:12:56 +0100

    amélioration de filtrage

--- src/views.py
+++ src/views.py
@@ -535,53 +535,50 @@ def mes_offres():
     """
     
 
-    #for r in get_reseaux_for_user(current_user):
-
-    les_reseaux = get_reseaux_for_user(current_user)
+    les_reseaux = get_reseaux_for_user(current_user) #les reseaux de l'utilisateur currrent 
     f_select_reseau = SelectRechercheOffreForm()
-    f_select_reseau.reseaux.choices = [(reseau.id_reseau, reseau.nom_reseau) for reseau in les_reseaux]
+    f_select_reseau.reseaux.choices = [(reseau.id_reseau, reseau.nom_reseau) for reseau in les_reseaux] #affichage  des reseaux de l'utilisateur currrent 
 
     proximité_date = SelectDateProximité()
-    proximité_date.proxi.choices = ["Plus Proche", "Moins Proche"]
+    proximité_date.proxi.choices = ["Plus Proche", "Moins Proche"] #afficage  de filtre de date d'expiration
 
-    if proximité_date.proxi.data == None :
+    if proximité_date.proxi.data == None : #Si pas de choix la choix de base est plus proche pour la date d'expiration
         proximité_date.proxi.default = "Plus Proche"
         proximité_date.process()
 
-    id_reseaux_elu =  f_select_reseau.reseaux.data
+    id_reseaux_elu =  f_select_reseau.reseaux.data #recupere l'information des reseaux choisi 
     les_reseaux_elu = []
 
 
-    proxi_elu = proximité_date.proxi.data
+    proxi_elu = proximité_date.proxi.data #recupere l'information de date d'expiration
 
    
-    if proximité_date.validate_on_submit() or f_select_reseau.validate_on_submit():
-        if proxi_elu == "Plus Proche": 
-                les_offres = Offre.query.filter_by(id_utilisateur=current_user.id_utilisateur).order_by(Offre.date_fin).all()
-        else:
+    if proximité_date.validate_on_submit() or f_select_reseau.validate_on_submit(): #Si Choix fait 
+        if proxi_elu == "Plus Proche":  #Si date plus proche 
                 les_offres = Offre.query.filter_by(id_utilisateur=current_user.id_utilisateur).order_by(Offre.date_fin.desc()).all()
+        else: #Si date moins proche
+                les_offres = Offre.query.filter_by(id_utilisateur=current_user.id_utilisateur).order_by(Offre.date_fin).all()
 
-        for r in id_reseaux_elu:
-            les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
-    else:
+        for id_r in id_reseaux_elu:
+            les_reseaux_elu.append(Reseau.query.get(id_r))
+    else: 
         les_offres = Offre.query.filter_by(id_utilisateur=current_user.id_utilisateur).order_by(Offre.date_fin).all()
 
-    if les_reseaux_elu != []:
-        les_offres = filtrage_des_offrres_par_reseux(les_reseaux_elu, les_offres)
-    else:
+    if les_reseaux_elu != []: #Si des reseux était choixi par utilisateur
+        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in les_reseaux_elu]
+        offres = [Offre.query.get(o.id_offre) for o in offre_reseau[0]] 
+        les_offres = offres  
+
+    else: #Si aucun choit était fait 
         
         offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in Utilisateur_Reseau.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()]
         offres = [Offre.query.get(o.id_offre) for o in offre_reseau[0]]
-        les_offres = offres
+
+        les_offres = list(set(les_offres) & set(offres)) #intersection des offres de utlisateur et les offres présante dans ces reseau
+
     return render_template('mes-offres.html', offres=les_offres,form=f_select_reseau,formd=proximité_date)
 
-def filtrage_des_offrres_par_reseux(les_reseaux_elu, les_offres):
-    offre_voulu = set()
-    for offre in les_offres:
-        for reseux_off in offre.les_reseaux:
-            if reseux_off.id_reseau in les_reseaux_elu:
-                offre_voulu.add(offre)
-    return list(offre_voulu)
+
 
 @app.route('/home/les-offres', methods=["POST","GET"])
 def les_offres():
@@ -607,22 +604,32 @@ def les_offres():
     les_reseaux_elu = []
     
     
+        
     if proximité_date.validate_on_submit() or f_select_reseau.validate_on_submit():
         if proxi_elu == "Plus Proche": 
-                les_offres = Offre.query.filter_by(etat="publiée").order_by(Offre.date_fin).all()
-        else:
                 les_offres = Offre.query.filter_by(etat="publiée").order_by(Offre.date_fin.desc()).all()
+        else:
+                les_offres = Offre.query.filter_by(etat="publiée").order_by(Offre.date_fin).all()
 
-        for r in id_reseaux_elu:
-            les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
+        for id_r in id_reseaux_elu:
+            print(Reseau.query.get(id_r))
+            les_reseaux_elu.append(Reseau.query.get(id_r))
     else:
         les_offres = Offre.query.filter_by(etat="publiée").order_by(Offre.date_fin).all()
 
 
     if les_reseaux_elu != []:
-        les_offres = filtrage_des_offrres_par_reseux(les_reseaux_elu, les_offres)
+        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau,).all() for reseau in les_reseaux_elu]
+        offres = [Offre.query.get(o.id_offre) for o in offre_reseau[0]] 
+        les_offres = list(set(les_offres) & set(offres))
     else: 
-        les_offres = filtrage_des_offrres_par_reseux(les_reseaux, les_offres)
+        
+        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in Utilisateur_Reseau.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()]
+            
+        if len(offre_reseau) != 0:
+            offres = [Offre.query.get(o.id_offre) for o in offre_reseau[0]] 
+            les_offres = list(set(les_offres) & set(offres)) #intersection des offres publiée et la liste des offres présante dans les reseaux de l'utilisateur current   
+
 
     
 
@@ -670,35 +677,43 @@ def mes_reponses():
     les_reseaux_elu = []
     
     if proximité_date.validate_on_submit() or f_select_reseau.validate_on_submit():
-        if proxi_elu == "Plus Proche": 
-                les_reponses = Reponse.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()
-        else:
-                les_reponses = Reponse.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()
+        
 
-        for r in id_reseaux_elu:
-            les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
-    else:
-        les_reponses = Reponse.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()
+        for id_r in id_reseaux_elu:
+            les_reseaux_elu.append(Reseau.query.get(id_r))
+    
 
 
     if les_reseaux_elu != []:
-        les_reponses = filtrage_des_reponses_par_reseux(les_reseaux_elu, les_reponses)
-    else:
-        les_reponses = filtrage_des_reponses_par_reseux(les_reseaux, les_reponses)
+        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in les_reseaux_elu]
+        offres = [Offre.query.get(o.id_offre) for o in offre_reseau[0]]
+        les_reponses = []
+        
+        
+        if proxi_elu == "Plus Proche":
+            les_rep = [Reponse.query.filter_by(id_offre=offre.id_offre, id_utilisateur=current_user.id_utilisateur).order_by(Reponse.date_fin).all() for offre in offres]
+            for offre in les_rep:
+                les_reponses += offre 
+                
+        else:
+            les_rep = [Reponse.query.filter_by(id_offre=offre.id_offre, id_utilisateur=current_user.id_utilisateur).order_by(Reponse.date_fin.desc()).all() for offre in offres]
+            for offre in les_rep:
+                les_reponses += offre 
+                
+
+        
+        
+
+    else: 
+        if proxi_elu == "Plus Proche":
+            les_reponses = Reponse.query.filter_by(id_utilisateur=current_user.id_utilisateur).order_by(Reponse.date_fin.desc()).all()
+        else: 
+            les_reponses = Reponse.query.filter_by(id_utilisateur=current_user.id_utilisateur).order_by(Reponse.date_fin.desc()).all()
+    
 
     return render_template('mes-reponses.html', reponses=les_reponses, form=f_select_reseau, formd=proximité_date)
     
 
-   
-    
-
-def filtrage_des_reponses_par_reseux(les_reseaux_elu, les_reponses):
-    rep_voulu = set()
-    for rep in les_reponses:
-        for reseux_off in rep.offre.les_reseaux:
-            if reseux_off.id_reseau in les_reseaux_elu:
-                rep_voulu.add(rep)
-    return list(rep_voulu)
 
 @app.route('/home/genre', methods=['GET','POST'])
 def genre():
