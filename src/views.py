c73ef9e739109ebcc7beefe146e7a8dc58ea244e
o22306966 <valentinhun@hotmail.fr>
2025-02-25 15:28:28 +0100

    avancement de modifier offre

--- src/views.py
+++ src/views.py
@@ -415,15 +415,19 @@ def rechercher():
         _type_: _description_
     """
 
-    
-@app.route('/home/creation-offre', methods=['GET','POST'])
+@app.route('/home/creation-offre', defaults={'id_offre': None}, methods=['GET','POST'])    
+@app.route('/home/creation-offre/<int:id_offre>', methods=['GET','POST'])
 @login_required
-def creation_offre():
+def creation_offre(id_offre=None):
     """Renvoie la page de création d'une offre
 
     Returns:
         creation-offre.html: Une page de création d'une offre
     """
+
+    o = Offre.query.get(id_offre) if id_offre else None
+    print(o,"oooooo")
+
     f = OffreForm()
     f.genre.choices = [(genre.id_genre, genre.nom_genre) for genre in Genre.query.all()]
 
@@ -435,150 +439,219 @@ def creation_offre():
     les_reseaux_elu = []
     
     if f.validate_on_submit(): # and f.validate(): # ! A implémenter quand on affiche les erreurs dans le formulaire
-        o = Offre()
-        o.nom_offre = f.nom_offre.data
-        o.description = f.description.data
-        o.date_limite = f.date_limite.data
-        o.budget = f.budget.data
-        o.cotisation_min = f.cotisation_min.data
-        o.capacite_max = f.capacite_max.data
-        o.capacite_min = f.capacite_min.data
-        #o.img = f.img.data
-        o.etat = "brouillon"
-        o.nom_loc = f.nom_loc.data
-        o.date_deb = f.date_deb.data
-        o.date_fin = f.date_fin.data
-        o.id_utilisateur = current_user.id_utilisateur
-        file_o = f.img.data
-        db.session.add(o)
-        db.session.commit()
-        id_offre = o.id_offre
-        if not file_o:
-            o.img = "0"
-        else:
-            file_path = os.path.join("src/static/img/offre", str(id_offre))
-            file_o.save(file_path)
-            o.img = id_offre
-        db.session.commit()
-        file = f.documents.data
-        if file:
-            d = Document() # ! pour l'instant il n'y a qu'un document par offre. Si ça marche pas, remplacer f.documents.data par list(f.documents.data) ou [f.documents.data]
-            filename = secure_filename(file.filename)
-            d.nom_doc = filename
-            d.id_offre = id_offre
-            db.session.add(d)
+        if o is None:
+            o = Offre()
+            o.nom_offre = f.nom_offre.data
+            o.description = f.description.data
+            o.date_limite = f.date_limite.data
+            o.budget = f.budget.data
+            o.cotisation_min = f.cotisation_min.data
+            o.capacite_max = f.capacite_max.data
+            o.capacite_min = f.capacite_min.data
+            #o.img = f.img.data
+            o.etat = "brouillon"
+            o.nom_loc = f.nom_loc.data
+            o.date_deb = f.date_deb.data
+            o.date_fin = f.date_fin.data
+            o.id_utilisateur = current_user.id_utilisateur
+            file_o = f.img.data
+            db.session.add(o)
             db.session.commit()
-            file_path = os.path.join("src/static/Documents", str(d.id_doc)+"-"+str(id_offre)) 
-            file.save(file_path)
-            o.docs = True
-        else:
-            o.docs = False
-        # for genre in f.genre.data: # ! pour l'instant il n'y a qu'un genre par offre. Si ��a marche pas, remplacer f.genre.data par list(f.genre.data) ou [f.genre.data]
-        g = Genre.query.get(f.genre.data)
-        g_o = Genre_Offre()
-        g_o.id_genre = g.id_genre
-        g_o.id_offre = id_offre
-        db.session.add(g_o)
-        db.session.commit()
-
-        for r in id_reseaux_elu:
-            les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
-            
-        for r in les_reseaux_elu:
-            o_r = Offre_Reseau()
-            o_r.id_reseau = r
-            o_r.id_offre = id_offre
-            db.session.add(o_r)
+            id_offre = o.id_offre
+            if not file_o:
+                o.img = "0"
+            else:
+                file_path = os.path.join("src/static/img/offre", str(id_offre))
+                file_o.save(file_path)
+                o.img = id_offre
             db.session.commit()
-        return redirect(url_for('mes_offres'))
-    return render_template('creation-offre.html',reseaux=les_reseaux, form=f, form_reseaux=f_select_reseau)
+            file = f.documents.data
+            if file:
+                d = Document() # ! pour l'instant il n'y a qu'un document par offre. Si ça marche pas, remplacer f.documents.data par list(f.documents.data) ou [f.documents.data]
+                filename = secure_filename(file.filename)
+                d.nom_doc = filename
+                d.id_offre = id_offre
+                db.session.add(d)
+                db.session.commit()
+                file_path = os.path.join("src/static/Documents", str(d.id_doc)+"-"+str(id_offre)) 
+                file.save(file_path)
+                o.docs = True
+            else:
+                o.docs = False
+            # for genre in f.genre.data: # ! pour l'instant il n'y a qu'un genre par offre. Si ��a marche pas, remplacer f.genre.data par list(f.genre.data) ou [f.genre.data]
+            g = Genre.query.get(f.genre.data)
+            g_o = Genre_Offre()
+            g_o.id_genre = g.id_genre
+            g_o.id_offre = id_offre
+            db.session.add(g_o)
+            db.session.commit()
 
-@app.route('/home/modification-offre/<int:id_offre>', methods=['GET','POST'])
-@login_required
-def modifier_offre(id_offre):
-    """Renvoie la page de création d'une offre
+            for r in id_reseaux_elu:
+                les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
 
-    Returns:
-        creation-offre.html: Une page de création d'une offre
-    """
-    o = Offre.query.get(id_offre)
+            for r in les_reseaux_elu:
+                o_r = Offre_Reseau()
+                o_r.id_reseau = r
+                o_r.id_offre = id_offre
+                db.session.add(o_r)
+                db.session.commit()
+            return redirect(url_for('mes_offres'))
+        else:
+            o.nom_offre = f.nom_offre.data
+            o.description = f.description.data
+            o.date_limite = f.date_limite.data
+            o.budget = f.budget.data
+            o.cotisation_min = f.cotisation_min.data
+            o.capacite_max = f.capacite_max.data
+            o.capacite_min = f.capacite_min.data
+            #o.img = f.img.data
+            o.etat = "brouillon"
+            o.nom_loc = f.nom_loc.data
+            o.date_deb = f.date_deb.data
+            o.date_fin = f.date_fin.data
+            o.id_utilisateur = current_user.id_utilisateur
+            file_o = f.img.data
+            db.session.commit()
+            if not file_o:
+                o.img = "0"
+            else:
+                file_path = os.path.join("src/static/img/offre", str(id_offre))
+                file_o.save(file_path)
+                o.img = id_offre
+            db.session.commit()
+            file = f.documents.data
+            if file:
+                d = Document()
+                filename = secure_filename(file.filename)
+                d.nom_doc = filename
+                d.id_offre = id_offre
+                db.session.commit()
+                file_path = os.path.join("src/static/Documents", str(d.id_doc)+"-"+str(id_offre)) 
+                file.save(file_path)
+                o.docs = True
+            else:
+                o.docs = False
+            # for genre in f.genre.data: # ! pour l'instant il n'y a qu'un genre par offre. Si ��a marche pas, remplacer f.genre.data par list(f.genre.data) ou [f.genre.data]
+            g = Genre.query.get(o.les_genres[0].id_genre)
+            # g_o = Genre_Offre()
+            g_o = Genre_Offre.query.filter_by(id_offre=id_offre).first()
+            g_o.id_genre = g.id_genre
+            g_o.id_offre = id_offre
+            db.session.commit()
 
-    f = OffreForm()
-    f.genre.choices = [(genre.id_genre, genre.nom_genre) for genre in Genre.query.all()]
+            for r in id_reseaux_elu:
+                les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
 
-    les_reseaux = [Reseau.query.get(res.id_reseau) for res in Utilisateur_Reseau.query.filter_by(id_utilisateur = current_user.id_utilisateur)]
-    f_select_reseau = SelectRechercheOffreForm()
-    f_select_reseau.reseaux.choices = [(reseau.id_reseau, reseau.nom_reseau) for reseau in les_reseaux]
-
-    id_reseaux_elu =  f_select_reseau.reseaux.data
-    les_reseaux_elu = []
+            for r in les_reseaux_elu:
+                o_r = Offre_Reseau()
+                o_r.id_reseau = r
+                o_r.id_offre = id_offre
+                db.session.commit()
+            print("azeazeaze")
+            return redirect(url_for('mes_offres'))
+    if o:
+        f.nom_offre.data = o.nom_offre
+        f.description.data = o.description
+        f.date_limite.data = o.date_limite
+        f.budget.data = o.budget
+        f.cotisation_min.data = o.cotisation_min
+        f.capacite_max.data = o.capacite_max
+        f.capacite_min.data = o.capacite_min
+        f.nom_loc.data = o.nom_loc
+        f.date_deb.data = o.date_deb
+        f.date_fin.data = o.date_fin
+        f.genre.data = o.les_genres[0].id_genre
+        f.img.data = o.img
+        # f.documents.data = o.les_documents[0].id_doc
     
-    if f.validate_on_submit(): # and f.validate(): # ! A implémenter quand on affiche les erreurs dans le formulaire    
-        o.nom_offre = f.nom_offre.data
-        o.description = f.description.data
-        o.date_limite = f.date_limite.data
-        o.budget = f.budget.data
-        o.cotisation_min = f.cotisation_min.data
-        o.capacite_max = f.capacite_max.data
-        o.capacite_min = f.capacite_min.data
-        #o.img = f.img.data
-        o.etat = "brouillon"
-        o.nom_loc = f.nom_loc.data
-        o.date_deb = f.date_deb.data
-        o.date_fin = f.date_fin.data
-        o.id_utilisateur = current_user.id_utilisateur
-        file_o = f.img.data
-        db.session.commit()
-        if not file_o:
-            o.img = "0"
-        else:
-            file_path = os.path.join("src/static/img/offre", str(id_offre))
-            file_o.save(file_path)
-            o.img = id_offre
-        db.session.commit()
-        file = f.documents.data
-        if file:
-            d = Document() # ! pour l'instant il n'y a qu'un document par offre. Si ça marche pas, remplacer f.documents.data par list(f.documents.data) ou [f.documents.data]
-            filename = secure_filename(file.filename)
-            d.nom_doc = filename
-            d.id_offre = id_offre
-            db.session.commit()
-            file_path = os.path.join("src/static/Documents", str(d.id_doc)+"-"+str(id_offre)) 
-            file.save(file_path)
-            o.docs = True
-        else:
-            o.docs = False
-        # for genre in f.genre.data: # ! pour l'instant il n'y a qu'un genre par offre. Si ��a marche pas, remplacer f.genre.data par list(f.genre.data) ou [f.genre.data]
-        g = Genre.query.get(f.genre.data)
-        g_o = Genre_Offre()
-        g_o.id_genre = g.id_genre
-        g_o.id_offre = id_offre
-        db.session.commit()
+    return render_template('creation-offre.html',reseaux=les_reseaux, form=f, form_reseaux=f_select_reseau, offre=o)
 
-        for r in id_reseaux_elu:
-            les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
+# @app.route('/home/modification-offre/<int:id_offre>', methods=['GET','POST'])
+# @login_required
+# def modifier_offre(id_offre):
+#     """Renvoie la page de création d'une offre
+
+#     Returns:
+#         creation-offre.html: Une page de création d'une offre
+#     """
+#     o = Offre.query.get(id_offre)
+
+#     f = OffreForm()
+#     f.genre.choices = [(genre.id_genre, genre.nom_genre) for genre in Genre.query.all()]
+
+#     les_reseaux = [Reseau.query.get(res.id_reseau) for res in Utilisateur_Reseau.query.filter_by(id_utilisateur = current_user.id_utilisateur)]
+#     f_select_reseau = SelectRechercheOffreForm()
+#     f_select_reseau.reseaux.choices = [(reseau.id_reseau, reseau.nom_reseau) for reseau in les_reseaux]
+
+#     id_reseaux_elu =  f_select_reseau.reseaux.data
+#     les_reseaux_elu = []
+    
+#     if f.validate_on_submit(): # and f.validate(): # ! A implémenter quand on affiche les erreurs dans le formulaire    
+#         o.nom_offre = f.nom_offre.data
+#         o.description = f.description.data
+#         o.date_limite = f.date_limite.data
+#         o.budget = f.budget.data
+#         o.cotisation_min = f.cotisation_min.data
+#         o.capacite_max = f.capacite_max.data
+#         o.capacite_min = f.capacite_min.data
+#         #o.img = f.img.data
+#         o.etat = "brouillon"
+#         o.nom_loc = f.nom_loc.data
+#         o.date_deb = f.date_deb.data
+#         o.date_fin = f.date_fin.data
+#         o.id_utilisateur = current_user.id_utilisateur
+#         file_o = f.img.data
+#         db.session.commit()
+#         if not file_o:
+#             o.img = "0"
+#         else:
+#             file_path = os.path.join("src/static/img/offre", str(id_offre))
+#             file_o.save(file_path)
+#             o.img = id_offre
+#         db.session.commit()
+#         file = f.documents.data
+#         if file:
+#             d = Document() # ! pour l'instant il n'y a qu'un document par offre. Si ça marche pas, remplacer f.documents.data par list(f.documents.data) ou [f.documents.data]
+#             filename = secure_filename(file.filename)
+#             d.nom_doc = filename
+#             d.id_offre = id_offre
+#             db.session.commit()
+#             file_path = os.path.join("src/static/Documents", str(d.id_doc)+"-"+str(id_offre)) 
+#             file.save(file_path)
+#             o.docs = True
+#         else:
+#             o.docs = False
+#         # for genre in f.genre.data: # ! pour l'instant il n'y a qu'un genre par offre. Si ��a marche pas, remplacer f.genre.data par list(f.genre.data) ou [f.genre.data]
+#         g = Genre.query.get(f.genre.data)
+#         g_o = Genre_Offre()
+#         g_o.id_genre = g.id_genre
+#         g_o.id_offre = id_offre
+#         db.session.commit()
+
+#         for r in id_reseaux_elu:
+#             les_reseaux_elu.append(f_select_reseau.reseaux.choices[int(r)-1][0])
             
-        for r in les_reseaux_elu:
-            o_r = Offre_Reseau()
-            o_r.id_reseau = r
-            o_r.id_offre = id_offre
-            db.session.commit()
-        return redirect(url_for('mes_offres'))
+#         for r in les_reseaux_elu:
+#             o_r = Offre_Reseau()
+#             o_r.id_reseau = r
+#             o_r.id_offre = id_offre
+#             db.session.commit()
+#         return redirect(url_for('mes_offres'))
 
-    f.nom_offre.data = o.nom_offre
-    f.description.data = o.description
-    f.date_limite.data = o.date_limite
-    f.budget.data = o.budget
-    f.cotisation_min.data = o.cotisation_min
-    f.capacite_max.data = o.capacite_max
-    f.capacite_min.data = o.capacite_min
-    f.nom_loc.data = o.nom_loc
-    f.date_deb.data = o.date_deb
-    f.date_fin.data = o.date_fin
-    f.genre.data = o.les_genres[0].id_genre
-    f.img.data = o.img
-    # f.documents.data = o.les_documents[0].id_doc
-    return render_template('creation-offre.html',reseaux=les_reseaux, form=f, form_reseaux=f_select_reseau)
+#     f.nom_offre.data = o.nom_offre
+#     f.description.data = o.description
+#     f.date_limite.data = o.date_limite
+#     f.budget.data = o.budget
+#     f.cotisation_min.data = o.cotisation_min
+#     f.capacite_max.data = o.capacite_max
+#     f.capacite_min.data = o.capacite_min
+#     f.nom_loc.data = o.nom_loc
+#     f.date_deb.data = o.date_deb
+#     f.date_fin.data = o.date_fin
+#     f.genre.data = o.les_genres[0].id_genre
+#     f.img.data = o.img
+#     # f.documents.data = o.les_documents[0].id_doc
+#     return render_template('creation-offre.html',reseaux=les_reseaux, form=f, form_reseaux=f_select_reseau)
 
 @app.route('/home/visualiser-reponses-offres') #! A MODIFIER QUAND LA PAGE DE L'OFFRE SERA CREEE
 def visualiser_offre():
