a1e353bc63fdbee564b5cb66171d093db48b5cc2
Joann45 <joann.raignault@gmail.com>
2025-02-27 14:48:35 +0100

    début fix filtrage (manque que le filtre de statut dans mes_offres)

--- src/offre/offre.py
+++ src/offre/offre.py
@@ -381,17 +381,14 @@ def les_offres(page=1):
 
     if proximite_date.validate_on_submit() or f_select_reseau.validate_on_submit():
         for id_r in id_reseaux_elu:
-            print(Reseau.query.get(id_r))
             les_reseaux_elu.append(Reseau.query.get(id_r))
     if les_reseaux_elu != []:
-        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau,).all() for reseau in les_reseaux_elu]
+        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in les_reseaux_elu]
     else: 
         
-        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in Utilisateur_Reseau.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()]
-             
-               
+        offre_reseau = [Offre_Reseau.query.filter_by(id_reseau=reseau.id_reseau).all() for reseau in Utilisateur_Reseau.query.filter_by(id_utilisateur=current_user.id_utilisateur).all()]         
     if len(offre_reseau) != 0: 
-        les_offres = Offre.query.filter(Offre.etat == "publiée",Offre.id_offre.in_([o_r.offre.id_offre for o_r in offre_reseau[0]]), Offre.date_limite > dt.date.today())
+        les_offres = Offre.query.filter(Offre.etat == "publiée",Offre.id_offre.in_([o_r[0].offre.id_offre for o_r in offre_reseau]), Offre.date_limite > dt.date.today())
     else:
         les_offres = []
     if proxi_elu == "Plus Proche":
